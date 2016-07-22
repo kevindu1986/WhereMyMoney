fefb1e2374f02e95a2fd62a2b3aec7c46660321d
kevin <kevindu1986@outlook.com>
2016-07-22 17:03:03 +0700

    Build RC 0.5.01 Add create table scripts

--- src/WhereMyMoney/Models/Database Scripts/CreateTables.sql
+++ src/WhereMyMoney/Models/Database Scripts/CreateTables.sql
@@ -1,19 +1,15 @@
-﻿USE [WhereMyMoney]
-GO
+﻿CREATE TABLE [dbo].[Tbl_Category](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [varchar](50) NOT NULL,
+	[UserID] [int] NOT NULL,
+	[Description] [ntext] NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_Tbl_Category] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-ALTER TABLE [dbo].[Tbl_Trace] DROP CONSTRAINT [FK_Tbl_Trace_Tbl_User]
-GO
-
-ALTER TABLE [dbo].[Tbl_Trace] DROP CONSTRAINT [FK_Tbl_Trace_Tbl_Currency]
-GO
-
-DROP TABLE [dbo].[Tbl_Trace]
-GO
-
-DROP TABLE [dbo].[Tbl_Currency]
-GO
-
-DROP TABLE [dbo].[Tbl_User]
 GO
 
 CREATE TABLE [dbo].[Tbl_Currency](
@@ -27,19 +23,21 @@ PRIMARY KEY CLUSTERED
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
 GO
 
 CREATE TABLE [dbo].[Tbl_User](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[UserName] [varchar](20) NOT NULL,
-	[Password] [varchar](50) NOT NULL,
+	[Password] [varchar](500) NOT NULL,
 	[IsActive] [bit] NOT NULL,
 	[IsAdmin] [bit] NOT NULL,
-PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK__Tbl_User__3214EC07A5ED3973] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
 GO
 
 CREATE TABLE [dbo].[Tbl_Trace](
@@ -48,6 +46,8 @@ CREATE TABLE [dbo].[Tbl_Trace](
 	[TraceDate] [date] NOT NULL,
 	[UserId] [int] NOT NULL,
 	[CurrencyId] [int] NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[TransactionType] [varchar](10) NOT NULL,
 	[Description] [ntext] NULL,
 	[IsActive] [bit] NOT NULL,
  CONSTRAINT [PK_Tbl_Trace] PRIMARY KEY CLUSTERED 
@@ -55,6 +55,21 @@ CREATE TABLE [dbo].[Tbl_Trace](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Tbl_Category]  WITH CHECK ADD  CONSTRAINT [FK_Tbl_Category_Tbl_User] FOREIGN KEY([UserID])
+REFERENCES [dbo].[Tbl_User] ([Id])
+GO
+
+ALTER TABLE [dbo].[Tbl_Category] CHECK CONSTRAINT [FK_Tbl_Category_Tbl_User]
+GO
+
+ALTER TABLE [dbo].[Tbl_Trace]  WITH CHECK ADD  CONSTRAINT [FK_Tbl_Trace_Tbl_Category] FOREIGN KEY([CategoryId])
+REFERENCES [dbo].[Tbl_Category] ([Id])
+GO
+
+ALTER TABLE [dbo].[Tbl_Trace] CHECK CONSTRAINT [FK_Tbl_Trace_Tbl_Category]
 GO
 
 ALTER TABLE [dbo].[Tbl_Trace]  WITH CHECK ADD  CONSTRAINT [FK_Tbl_Trace_Tbl_Currency] FOREIGN KEY([CurrencyId])
@@ -70,3 +85,5 @@ GO
 
 ALTER TABLE [dbo].[Tbl_Trace] CHECK CONSTRAINT [FK_Tbl_Trace_Tbl_User]
 GO
+
+
